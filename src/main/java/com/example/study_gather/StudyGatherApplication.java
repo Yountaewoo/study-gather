996923ba843b5133713d8dd7b3efd75a63a4164d
youn <tty7797@naver.com>
2026-01-05 16:42:11 +0900

    first commit

---
+++ src/main/java/com/example/study_gather/StudyGatherApplication.java
@@ -0,0 +1,13 @@
+package com.example.study_gather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyGatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyGatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/study_gather/StudyGatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.study_gather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyGatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
